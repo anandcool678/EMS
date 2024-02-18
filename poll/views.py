a2f0015315911ef6ba8a0888f9486f00c4335b37
anandcool678 <anand201101dwivedi@gmail.com>
2024-02-18 23:37:15 +0530

    Changes

--- poll/views.py
+++ poll/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from poll.models import *
+from django.http import Http404
 
-# Create your views here.
+
+def index(request):
+    context = {}
+    questions = Question.objects.all()
+    context['title'] = 'polls'
+    context['questions'] = questions
+    return render(request, 'polls/index.html', context)
+
+
+def details(request, id=None):
+    context = {}
+    try:
+        questions = Question.objects.get(id=id)
+    except:
+        raise Http404
+    context['questions'] = questions
+    return render(request, 'polls/details.html', context)
